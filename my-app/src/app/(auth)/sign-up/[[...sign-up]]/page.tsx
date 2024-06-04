bc59ee9e42ee713e24fe30321854bfd7f7dbdf69
6426mohaN <getiyenahom751@gmail.com>
2024-06-04 21:44:35 +0300

    added some home functionality

---
+++ my-app/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignUp} from '@clerk/nextjs'
+
+const SignUpPage = () => {
+  return (
+<main className=' flex w-full h-screen justify-center items-center'>
+    <SignUp />
+</main>
+  )
+}
+
+export default SignUpPage
